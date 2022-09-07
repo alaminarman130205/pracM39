b92c44ceb55d75256e164d32d8c51cc4e9ae7dbb
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-08 00:06:25 +0600

    == and === differnece

---
+++ java.js
@@ -0,0 +1,13 @@
+const  first  = 2; 
+const second  = '2'; 
+if(first === second ){
+    console.log('value ar equal');
+}else{
+    console.log('value ar not equal');
+}
+
+if(first == second ){
+    console.log('value ar equal');
+}else{
+    console.log('value ar not equal');
+}
